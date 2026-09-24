9073bc7430ff09a207662dfd0a371342ef7887c9
Lasantha Mudalige <lasantha1130@gmail.com>
2026-09-24 19:38:54 +0530

    add comments for readability

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,23 @@ use std::io;
 fn main() {
     let mut input = String::new();
 
+    // Show all the operations available to the user and get the operation as an integer
     show_operations();
     println!("Enter the operation: ");
     io::stdin().read_line(&mut input).unwrap();
     let operation: i64 = input.trim().parse().unwrap();
 
+    // Clear the input after every entry
     input.clear();
 
+    // Get the first number from the user
     println!("Enter first number: ");
     io::stdin().read_line(&mut input).unwrap();
     let num1: f64 = input.trim().parse().unwrap();
 
     input.clear();
 
+    // Get the first number from the user
     println!("Enter second number: ");
     io::stdin().read_line(&mut input).unwrap();
     let num2: f64 = input.trim().parse().unwrap();
@@ -34,7 +38,11 @@ fn main() {
 }
 
 fn show_operations() {
-    println!("Please 1 or for following operations\n1. Addtion\n2. Subtraction");
+    println!(
+        "Please 1 or 2 for following operations
+    1. Addtion
+    2. Subtraction"
+    );
 }
 
 fn add(num1: f64, num2: f64) -> f64 {
